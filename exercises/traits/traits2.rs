ef05f3c6e10c053cb0df79572832409c29cba314
CandySunPlus <s@sfmblog.cn>
2021-08-10 17:33:09 +0000

    commit more

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,25 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+
+impl AppendBar for String {
+    fn append_bar(self) -> Self {
+        self + "Bar"
+    }
+}
+
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".into());
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
